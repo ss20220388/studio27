d60205fd25fc9d101d996bb4791a7ac542af834f
anastasijastaletovic <anstasijas002@gmail.com>
2026-02-16 14:23:05 +0100

    First Commit-Initialization

---
+++ server/src/test/java/com/server/studio27/Studio27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.studio27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Studio27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
