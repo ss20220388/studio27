f11137d9a8c2537c506995893226b1a7e1553800
mlstom <tm220124d@student.etf.bg.ac.rs>
2026-02-17 13:24:40 +0100

    Dodata kurs Kontroler treba sve da se podesi

--- server/src/main/java/com/server/studio27/controllers/KursController.java
+++ server/src/main/java/com/server/studio27/controllers/KursController.java
@@ -1,5 +1,19 @@
 package com.server.studio27.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.server.studio27.models.Kurs;
+@Service
 public class KursController {
+
+    public List<Kurs> getAllKursevi() {
+        List<Kurs> kursevi = new ArrayList<>();
+        kursevi.add(new Kurs(1, "Java Programming", "Learn Java from scratch", null, 100));
+        kursevi.add(new Kurs(2, "Web Development", "Learn how to build websites", null, 150));
+        return kursevi;
+    }
 
 }
