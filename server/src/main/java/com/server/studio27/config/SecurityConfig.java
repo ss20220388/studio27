2fcd53a4b5a261bdee2977ca039db0abb1b7b4f7
ss20220388 <stojanovicstefana157@gmail.com>
2026-02-22 21:32:50 +0100

    opet push

--- server/src/main/java/com/server/studio27/config/SecurityConfig.java
+++ server/src/main/java/com/server/studio27/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/kursevi/**").permitAll()
                         .requestMatchers("/api/kursevi-sa-lekcijama/**").permitAll()
+                        .requestMatchers("/api/download-file").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
