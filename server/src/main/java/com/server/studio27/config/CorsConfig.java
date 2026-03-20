41652a2f90b47bacb50475f8f4801f314a369071
mlstom <tm220124d@student.etf.bg.ac.rs>
2026-03-20 21:37:57 +0100

    Promena server

--- server/src/main/java/com/server/studio27/config/CorsConfig.java
+++ server/src/main/java/com/server/studio27/config/CorsConfig.java
@@ -12,15 +12,15 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins(
                         "http://studio27.rs",
-                        "http://studio27.rs:3000",
                         "http://admin.studio27.rs",
-                        "http://admin.studio27.rs:4000",
                         "http://app.studio27.rs",
-                        "http://app.studio27.rs:5000",
                         "http://localhost:3000",
                         "http://localhost:4000",
                         "http://localhost:5000",
-                        "http://localhost:8080"
+                        "http://localhost:8080",
+                        "http://dev.27archviz.com",
+                        "http://admin.dev.27archviz.com",
+                        "http://app.dev.27archviz.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
