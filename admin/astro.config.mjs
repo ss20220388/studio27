53378234dc8ec44e10dbd16861acc60416eafcf5
mlstom <tm220124d@student.etf.bg.ac.rs>
2026-03-21 22:15:40 +0100

    admin

--- admin/astro.config.mjs
+++ admin/astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
     server: {
       allowedHosts: [
-        'admin.studio27.rs',"admin.dev.archviz.com"
+        'admin.studio27.rs',"admin.dev.27archviz.com"
       ],
       proxy: {
         '/api': {
