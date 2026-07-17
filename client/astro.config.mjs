1f69159a5a8e3bee8a927760c07b89895f7eede0
mlstom <tm220124d@student.etf.bg.ac.rs>
2026-07-17 10:32:06 +0200

    Sitemap

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import node from "@astrojs/node";
-
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    integrations: [react()],
+    integrations: [react(), sitemap()],
     adapter: node({
     mode: "standalone"
     }),
